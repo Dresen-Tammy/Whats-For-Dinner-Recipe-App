618a968d8c78f25708eb5385161fafb22fe8e96b
Tammy Dresen <dre15003@byui.edu>
2018-06-30 23:00:18 -0700

    register page

--- logic.js
+++ logic.js
@@ -169,6 +169,61 @@ logic.viewRecipe = function(req, res) {
 
 
 
+logic.addFavorite = function(req, res) {
+    console.log('addingFavorite');
+    var chef_id = req.body.chef_id;
+    var f2f_rid = req.body.f2f_rid;
+    var recipe_name = req.body.recipe_name;
+    var imageurl = req.body.imageurl;
+    var source = req.body.source;
+    var ingredients = req.body.ingredients;
+    // check to make sure vars are not empty
+     if (chef_id == undefined || f2f_rid == undefined || recipe_name == undefined || imageurl == undefined || source == undefined || ingredients == undefined) {
+        res.status(500).json({success:false, message: "Error adding to favorites 1."});
+    } else {
+        // check to see if recipe is in database.
+        access.getRecipeFromDb(f2f_rid, function(err, result) {
+            if (err) {
+                res.status(500).json({success:false, message: "error adding to favorites 2."});
+            } else if (result[0] !== undefined) {
+                let recipe_id = result[0].id;
+                console.log(recipe_id);
+                console.log(chef_id);
+                access.setFavoriteInDb(recipe_id, chef_id, function(err, result) {
+                    if (err) {
+                        res.status(500).json({success:false, message: "Error adding to favorites 3."});
+                    } else {
+                        res.json({success:true, message: "Added to favorites"})
+                    }
+                })     
+            
+            } else {   
+                // if not in database, add it, and save the recipe_id.
+                console.log("not in database");
+                access.setRecipeInDb(f2f_rid, recipe_name, imageurl, source, ingredients, function(err, result) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+
+                        let recipe_id = result.rows[0].id;
+                        console.log(recipe_id);
+                        console.log(chef_id);
+                        access.setFavoriteInDb(recipe_id, chef_id, function(err, result) {
+                            if (err) {
+                                res.status(500).json({success:false, message: "Error adding to favorites 3."});
+                            } else {
+                                res.json({success:true, message: "Added to favorites"})
+                            }
+                        })     
+                    }
+                })
+            }
+        
+        })
+    }
+}
+
+
 
 
 module.exports = logic;
