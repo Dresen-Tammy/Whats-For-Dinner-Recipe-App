7cb3b5790ecf776d18e6e90538f064929c774f0d
Tammy Dresen <dre15003@byui.edu>
2018-07-09 23:46:42 -0700

    waiting on hash

--- logic.js
+++ logic.js
@@ -2,9 +2,9 @@ const access = require('./access.js');
 const fetch = require('node-fetch');
 const http = require('http');
 //const bcrypt = require('bcrypt');
-var crypto = require('crypto'),
-    algorithm = 'aes-256-ctr',
-    password = '5th7LreP';
+//var crypto = require('crypto'),
+  //  algorithm = 'aes-256-ctr',
+  //  password = '5th7LreP';
 
 const session = require('express-session');
 var logic = {};
@@ -24,10 +24,11 @@ logic.login = function (req, response, next) {
         } else {
             console.log(pword);
             console.log(result[0].password);
-            var decipher = crypto.createDecipher(algorithm, password)
-            var dec = decipher.update(result[0].password, 'hex', 'utf8')
-            dec += decipher.final('utf8');
-            if (req.params.password === dec) {
+           // var decipher = crypto.createDecipher(algorithm, password)
+           // var dec = decipher.update(result[0].password, 'hex', 'utf8')
+           // dec += decipher.final('utf8');
+           // if (req.params.password === dec) {
+               if (req.params.password == result[0].password) { 
                     req.session.chef_id = result[0].id;
                     response.json({chef_id: result[0].id})
                 } else {
@@ -40,20 +41,18 @@ logic.login = function (req, response, next) {
 logic.register = function(req,res) {
     console.log('registering');
     var username = req.body.username;
-    var pword = req.bod;
-    if (username === null || pword === null || username === "" || pword === "") {
-        res.status(500).json({success:false, message: "Fill in all fields."});
-    }
+    var pword = req.body.password;
     access.getPersonFromDb(username, function(err, result) {
         if (err) {
             res.status(500).json({success:false, message: "error registering."});
         } else if (result === undefined) {
             res.status(400).json({success:false, message: "Username already registered."});
         } else {
-                var cipher = crypto.createCipher(algorithm, password);
-                var crypted = cipher.update(req.body.password, 'utf8', 'hex')
-                crypted += cipher.final('hex');
-                access.setPersonInDb(username, crypted, function(err, result) {
+                //var cipher = crypto.createCipher(algorithm, password);
+                //var crypted = cipher.update(req.body.password, 'utf8', 'hex')
+                //crypted += cipher.final('hex');
+                //access.setPersonInDb(username, crypted, function(err, result) {
+                    access.setPersonInDb(username, req.body.password, function(err, result) {
                         if (err) {
                             res.status(500).json({success:false, message: "Error registering."}); 
                         } else {
